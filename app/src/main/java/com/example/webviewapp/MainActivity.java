a2b3bfde4af810365d7825f662090d5eda430d1f
b21willu <b21willu@student.his.se>
2022-04-07 13:50:53 +0200

    Removed code that was for assignment2

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -8,26 +8,11 @@ import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Add a private member variabel
-    private WebView myWebView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        // Instantiate WebView
-        WebView myWebView = (WebView) findViewById(R.id.my_webview);
-
-        // Enable Javascript to our WebViewClient
-        myWebView.getSettings().setJavaScriptEnabled(true);
-
-        // Load external url
-        myWebView.loadUrl("https://scio.his.se");
-
-        // Sets WebView element active
-        setContentView(myWebView);
     }
 }
